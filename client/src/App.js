7db3b2cee916d4802a2a399206b3fb53d67ffe82
Samikmalhotra <samikmalhotra66@gmail.com>
2021-07-25 06:20:44 +0530

    added landing

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
+
 import './App.css';
+import LandingLayout from './layouts/LandingLayout';
+import SkillsLayout from './layouts/SkillsLayout';
+import Skills from './components/Skills'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <LandingLayout/>
+      <SkillsLayout/>
+      <Skills/>
     </div>
   );
 }
