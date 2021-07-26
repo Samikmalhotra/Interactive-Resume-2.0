95cbff4a6fe1ce06729f47b4d202e25d4f1b4ad9
Samikmalhotra <samikmalhotra66@gmail.com>
2021-07-27 04:30:26 +0530

    updated contact

--- client/src/components/Achievements.jsx
+++ client/src/components/Achievements.jsx
@@ -1,5 +1,7 @@
 import React,{Fragment} from 'react'
-
+import {Row,Col} from 'react-bootstrap'
+import achievement from '../img/Achievement-bro.svg'
+import '../css/achievement.css'
 const Achievements = () => {
     return (
         <Fragment>
@@ -7,6 +9,14 @@ const Achievements = () => {
             <h2 className="section-quote">"I was raised to treat the janitor with
 the same respect as that of a CEO"</h2>
             <hr/>  
+            <Row>
+                <Col lg={8} md={8}>
+
+                </Col>
+                <Col>
+                <img src={achievement} alt="achievement" className="achievement-img"/>
+                </Col>
+            </Row>
         </Fragment>
     )
 }
--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { Fragment } from 'react'
 import { Form } from 'react-bootstrap'
 import {TextField} from '@material-ui/core'
+import {Row, Col} from 'react-bootstrap'
+import contact from '../img/Mail sent-amico.svg'
 
 const Footer = () => {
     return (
@@ -9,10 +11,19 @@ const Footer = () => {
             <h2 className="section-heading">Contact</h2>
             <h2 className="section-quote">"A simple hello could lead to a million things"</h2>
             <hr/>  
+            <Row>
+                <Col lg={4} md={4}>
+                <img src={contact} alt="contact" className="contact-img"/>
+
+                </Col>
+                <Col>
 
                 <TextField label="Name"></TextField>
                 <TextField label="Email"></TextField>
                 <TextField label="Message"></TextField>
+                </Col>
+            </Row>
+
         </Fragment>
     )
 }
