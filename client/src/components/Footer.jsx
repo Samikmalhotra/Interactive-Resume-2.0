9c46f236578460ccfd3fca32091e07d2fc8dd6ff
Samikmalhotra <samikmalhotra66@gmail.com>
2021-07-26 22:26:18 +0530

    added project stylesheet

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { Fragment } from 'react'
+import { Form } from 'react-bootstrap'
+import {TextField} from '@material-ui/core'
 
 const Footer = () => {
     return (
@@ -7,6 +9,10 @@ const Footer = () => {
             <h2 className="section-heading">Contact</h2>
             <h2 className="section-quote">"A simple hello could lead to a million things"</h2>
             <hr/>  
+
+                <TextField label="Name"></TextField>
+                <TextField label="Email"></TextField>
+                <TextField label="Message"></TextField>
         </Fragment>
     )
 }
--- client/src/components/Projects.jsx
+++ client/src/components/Projects.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import { Fragment } from 'react'
+import {Row, Col} from 'react-bootstrap'
+import '../css/projects.css'
+import {Button} from '@material-ui/core'
 
 const Projects = () => {
     return (
@@ -8,6 +11,40 @@ const Projects = () => {
             <h2 className="section-quote">"Time is the scarcest resource and unless it
 is managed nothing else can be managed"</h2>
             <hr/>  
+            <Row>
+                <Col xs={12} sm={6} md={6} className="project-col">
+                    <div className='project-item'>
+                    <i></i>
+                    <h4>Project Name</h4>
+                    <p>Project description</p>
+                    </div>
+                </Col>
+                <Col xs={12} sm={6} md={6} className="project-col">
+                <div className='project-item'>
+                    <i></i>
+                    <h4>Project Name</h4>
+                    <p>Project description</p>
+                    </div>
+                </Col>
+                <Col xs={12} sm={6} md={6} className="project-col">
+                <div className='project-item'>
+                    <i></i>
+                    <h4>Project Name</h4>
+                    <p>Project description</p>
+                    </div>
+                </Col>
+                <Col xs={12} sm={6} md={6} className="project-col">
+                <div className='project-item'>
+                    <i></i>
+                    <h4>Project Name</h4>
+                    <p>Project description</p>
+                    </div>
+                </Col>
+            </Row>
+
+            <Button className='project-btn'>More Projects</Button>
+
+
         </Fragment>
     )
 }
