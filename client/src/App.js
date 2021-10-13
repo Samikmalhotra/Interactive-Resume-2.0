18986e3fc1af1eaf5f67c60653fcd5d3063e77ce
Samikmalhotra <samikmalhotra66@gmail.com>
2021-10-13 23:44:01 +0530

    added slick

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,16 @@
-
 import './App.css';
 import LandingLayout from './layouts/LandingLayout';
 import SkillsLayout from './layouts/SkillsLayout';
 import ProjectsLayout from './layouts/ProjectLayout';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
 function App() {
   return (
-    <div className="App">
-      <LandingLayout/>
-      <SkillsLayout/>
-      <ProjectsLayout/>
+    <div className='App'>
+      <LandingLayout />
+      <SkillsLayout />
+      <ProjectsLayout />
     </div>
   );
 }
